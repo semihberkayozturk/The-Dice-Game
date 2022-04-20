8b04e07025d73c9cdfa5201d8cb678521609dc7e
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2022-04-20 11:15:17 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1 = Math.floor((Math.random() * 6)) + 1;
+var randomNumber2 = Math.floor((Math.random() * 6)) + 1;
+
+var image1 = "images/dice" + randomNumber1 + ".png";
+var image2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".dice .img1").setAttribute("src", image1);
+document.querySelector(".dice .img2").setAttribute("src", image2);
+
+if (randomNumber1 > randomNumber2) {
+    document.getElementById("win_lose").innerHTML = "🏆 Player 1 Wins";
+} else if (randomNumber1 < randomNumber2) {
+    document.getElementById("win_lose").innerHTML = "Player 2 Wins 🏆";
+} else  {
+    document.getElementById("win_lose").innerHTML = "Draw";
+}
